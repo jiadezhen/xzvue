178872c56544a9f499a5bc243c987ea1e6e0f6b6
jiadezhen <476024117@qq.com>
2020-09-26 11:48:48 +0800

    引入axios及初期页面准备(APP.vue)中去除页眉

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
+
+//配置axios请求的基础路径
+axios.defaults.baseURL=axios;
+//将axios放入vue的原型对象中
+Vue.prototype.axios=axios;
+//结果:将来所有组件中都可用this.axios发送请求
 
 Vue.config.productionTip = false
 
